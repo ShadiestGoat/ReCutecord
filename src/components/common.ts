9b4865deedf5faa8f942bf694ae99ea507c24636
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-08-06 03:55:24 +0100

    Make Settings a partial

--- src/components/common.ts
+++ src/components/common.ts
@@ -5,13 +5,13 @@ export type settingsStringKeys = "Users" | "Channels" | "Guilds";
 export type settingUtil<T extends string> = `good${T}` | `bad${T}`;
 export type SettingsString = Record<settingUtil<settingsStringKeys>, string>;
 export type SettingsArray = Record<settingUtil<settingsArrayKeys>, string[]>;
-export type Settings = SettingsString &
+export type Settings = Partial<SettingsString &
   SettingsArray & {
     respectMutedChannels: boolean;
     respectMutedCategories: boolean;
     respectMutedGuilds: boolean;
     notifyIfFocused: boolean;
-  };
+}>
 
 export const defaultSettings = {
   badChannels: "",
