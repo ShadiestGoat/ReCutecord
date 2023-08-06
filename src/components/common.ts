eaf06e3bbc5385cef9a83fe1000c3c5410ce1424
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-08-06 04:11:37 +0100

    follow PascalCase typing convention

--- src/components/common.ts
+++ src/components/common.ts
@@ -1,10 +1,10 @@
 import { settings } from "replugged";
 
-export type settingsArrayKeys = "Phrases";
-export type settingsStringKeys = "Users" | "Channels" | "Guilds";
-export type settingUtil<T extends string> = `good${T}` | `bad${T}`;
-export type SettingsString = Record<settingUtil<settingsStringKeys>, string>;
-export type SettingsArray = Record<settingUtil<settingsArrayKeys>, string[]>;
+export type SettingsArrayKeys = "Phrases";
+export type SettingsStringKeys = "Users" | "Channels" | "Guilds";
+export type SettingUtil<T extends string> = `good${T}` | `bad${T}`;
+export type SettingsString = Record<SettingUtil<SettingsStringKeys>, string>;
+export type SettingsArray = Record<SettingUtil<SettingsArrayKeys>, string[]>;
 export type Settings = Partial<
   SettingsString &
     SettingsArray & {
