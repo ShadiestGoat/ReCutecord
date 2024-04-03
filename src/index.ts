d3a4bb914040751c225614ae343f35fc2ca75488
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-04-03 03:16:51 +0100

    Format <3

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ let stopDoingFunnyBusiness = false;
 
 function doFunnyBusiness(): void {
   if (stopDoingFunnyBusiness) {
-    stopDoingFunnyBusiness = false
+    stopDoingFunnyBusiness = false;
     return;
   }
 
