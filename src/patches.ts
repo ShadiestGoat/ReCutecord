2063ee6768bb7736e781cb5a419ea5f164e12f9f
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2025-04-20 00:48:00 +0100

    Fix patches (hopefully more future proof?)

--- src/patches.ts
+++ src/patches.ts
@@ -5,21 +5,24 @@ const PLUGIN = `replugged.plugins.plugins.get("eu.shadygoat.cutecord")`;
 export default [
   {
     // &&p.getTextChatNotificationMode()===G.Ypu.ENABLED&&!N.Z.disableNotifications
-    find: /\.getTextChatNotificationMode.+?\.disableNotifications/,
+    find: /MESSAGE_CREATE:/,
     replacements: [
       // Initial notif logic
+      // if(!T&&!S||d.type===J.uaV.CHANGELOG&&(null==d.changelog_id||E.Z.latestChangelogId()!==d.changelog_id))return!1;
       {
-        match: /let [a-zA-Z]+?=\(.+?CHANGELOG.+?return!1;/,
-        replace: `if (!(${PLUGIN}.exports?.shouldNotify(arguments[0]) ?? false))return!1;`,
+        match: /if(?!.*if.+?CHANGELOG)\(.+?CHANGELOG.+?return!1/,
+        replace: `console.log("test");if (!(${PLUGIN}.exports?.shouldNotify(arguments[0]) ?? false))return!1;`,
       },
       // Always enable sound - we know for sure that we're gonna show notifications :3
       {
-        match: /;let ([A-Za-z]+?)=.?[a-zA-Z.]+\.isSoundDisabled\(.+?\)/,
-        replace: ";let $1=true",
+        match: /!(?!.*!.*isSoundDisabled).+?\.isSoundDisabled\(.*?\)/,
+        replace: "true",
       },
-      // Notification logic part 2
+      // Notification logic part 2 - always call notification noise
+      // ;if(S&&(p&&q.Z.playNotificationSound("message3",.4,y),!ti)||!T)return!1;let D=n(808506).default,U=n(624864).Z,{OverlayNotificationDisabledSetting:R}=n(486016);if(null!=D.getFocusedPID()&&!U.isNotificationDisabled(R.TEXT_CHAT)&&!H.Z.disableNotifications)return!1
       {
-        match: /;if\([a-zA-Z]+?&&.+?message3.+?!1;(?=let\{)/,
+        match:
+          /;if(?<=canUseCustomNotificationSounds.*)\(.+?&&.+?\.playNotificationSound\(.+?\).+?disableNotifications.+?!1/,
         replace: ";",
       },
     ],
